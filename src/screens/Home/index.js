09d52357798d37d8b8a9d7942077f075aaa2df6c
Pedro Henrique <phandradesa@gmail.com>
2023-09-12 17:51:03 -0300

    [Trabalho React Native - 002] - Construção da Home, incrementado uma imagem e css pra test

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,78 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import PrimeVideoLogo from "../../assets/prime_video.png";
+import AmazonLogo from "../../assets/amazon_logo.png";
+
+import MovieTheWhell from "../../assets/movies/the_wheel_of_time.png";
 
 export const Home = () => {
-    return <Text>Home</Text>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.primeLogoImg} source={PrimeVideoLogo} />
+        <Image style={styles.anazonLogoImg} source={AmazonLogo} />
+      </View>
+
+
+      <View style={styles.category}>
+        <TouchableOpacity>
+          <Text style={styles.categoryText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.categoryText}>Tv Shows</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.categoryText}>Movies</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.categoryText}>KIds</Text>
+        </TouchableOpacity>
+      </View>
+
+        <TouchableOpacity style={styles.movieImageThumbnail}>
+            <Image source={MovieTheWhell} style={styles.movieImage}/>
+        </TouchableOpacity>
+
+
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#232f3e",
+    alignItems: "flex-start",
+  },
+
+  header: {
+    width: "100%",
+    paddingTop: 80,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  amazonLogoImg: {
+    marginTop: -32,
+    marginLeft: 30,
+  },
+
+  category: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 30,
+    marginBottom: 15,
+  },
+
+  categoryText: {
+    fontSize: 15,
+    fontWeight: "750",
+    color: "#FFF",
+  },
+
+  movieImageThumbnail: {
+    width:"100%",
+    alignItems:"center",
+  },
+});
